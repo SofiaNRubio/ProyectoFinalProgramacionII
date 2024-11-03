23fd881abe141d0fbf5b30871993b3d1399bf3e7
Miguel Nicolás Basualdo Martinez <niicomartiinez22@gmail.com>
2024-11-03 20:41:12 -0300

    El Eze se la come

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
+<<<<<<< HEAD
 from flask import Flask
 from routes import configure_routes
 
 app = Flask(__name__)
 
 if __name__ == '__main__':
+=======
+from flask import Flask,  render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/hello")
+def hello():
+    return "Hola Mundo"
+
+@app.route("/hello/<username>")
+def show_name(username):
+    return render_template("index.html", username=username)
+
+
+if __name__ == "__main__":
+>>>>>>> f21b19b (El Eze se la come)
     app.run(debug=True)
