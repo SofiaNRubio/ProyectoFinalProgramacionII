51df6c3452b7bd89021cda751755de360883ebc7
juli <anzorenasotelojuli@gmail.com>
2024-11-06 02:06:02 -0300

    avance con agregar
    me tira un error cuando termino de subir la prenda, etc y aprieto agregar

--- app.py
+++ app.py
@@ -39,19 +39,22 @@ def index():
 @app.route("/process", methods=["POST"])
 def process():
     # Obtener el valor de 'prenda' del formulario
-    prenda = request.form.get("prenda")
-    
+    producto = request.form.get("producto", '')
+    cantidad=request.form.get("cantidad", '')
+    precio=request.form.get("precio", '')
+    talle=request.form.get("talle", '')
+    colores=request.form.get("colores", '')
     # Comprobar si 'prenda' tiene un valor
-    if prenda:
+    if producto:
         # Insertar el valor en la base de datos
-        sql_query_insert = "INSERT INTO Stock (prenda) VALUES (%s)"
-        values = (prenda,)
+        sql_query_insert = "INSERT INTO Stock (producto, cantidad, precio, talle, colores) VALUES (%s, %s, %s, %s,%s)"
+        values = (producto, cantidad, precio, talle, colores)
         cursor.execute(sql_query_insert, values)
 
         # Confirmar cambios en la base de datos
         mydb.commit()
 
-        return f"La prenda agregada es: {prenda}"
+        return f"La prenda agregada es: {producto},{cantidad}, {precio}, {talle}, {colores}"
     else:
         return "No se ha proporcionado ninguna prenda."
 
--- db.py
+++ db.py
@@ -1,6 +1,33 @@
 import mysql.connector
-from mysql.connector import Error
+import app
+#from mysql.connector import Error
 
+mydb = mysql.connector.connect(
+host="localhost",
+user="juli",
+password="45875403", # Cambiar por tu contraseña
+database="easily_store" # Nombre de la base de datos
+)
+
+# OBjeto cursor que nos permite interactuar con la base de datos
+cursor = mydb.cursor()
+sql_query = "Select * from Stock" 
+
+cursor.execute(sql_query) #query, consulta sql (update, delete...)
+
+resultado = cursor.fetchall() # fetchall devuelve todas las instancias 
+print(resultado)
+
+"""
+# Insertar el valor en la base de datos
+sql_query_insert = "INSERT INTO Stock (producto, cantidad, precio, talle, colores) VALUES (%s, %s, %s, %s,%s)"
+values = (producto, cantidad, precio, talle, colores)
+cursor.execute(sql_query_insert, values)
+
+        # Confirmar cambios en la base de datos
+mydb.commit() """
+
+"""
 def create_connection():
     connection = None
     try:
@@ -14,4 +41,5 @@ def create_connection():
     except Error as err:
         print(f"Error al conectar a MySQL: '{err}'")
 
-    return connection
+    return connection 
+    """
