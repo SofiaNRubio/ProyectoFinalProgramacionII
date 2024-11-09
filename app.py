52fedca8918931cdcc472543ce9b41dbcb0081da
SofiaNRubio <sofianrubio8@gmail.com>
2024-11-09 12:47:41 -0300

    Funcionaaa

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ app.secret_key = "jhfgk"
 mydb = mysql.connector.connect(
     host="localhost",
     user="juli",
-    password="45875403",
+    password="S0f1anrub10.",
     database="easily_store"
 )
 cursor = mydb.cursor()
@@ -41,21 +41,21 @@ def index():
 def process():
     # Obtener el valor de 'prenda' del formulario
     producto = request.form.get("producto", '')
-    cantidad=request.form.get("cantidad", '')
-    precio=request.form.get("precio", '')
-    talle=request.form.get("talle", '')
-    colores=request.form.get("colores", '')
+    cantidad = request.form.get("cantidad", '')
+    precio = request.form.get("precio", '')
+    talle = request.form.get("talle", '')
+    colores = request.form.get("colores", '')
     # Comprobar si 'prenda' tiene un valor
     if producto:
         # Insertar el valor en la base de datos
-        sql_query_insert = "INSERT INTO Stock (producto, cantidad, precio, talle, colores) VALUES (%s, %s, %s, %s,%s)"
+        sql_query_insert = "INSERT INTO Stock (producto, cantidad, precio, talle, colores) VALUES (%s, %s, %s, %s, %s)"
         values = (producto, cantidad, precio, talle, colores)
         cursor.execute(sql_query_insert, values)
 
         # Confirmar cambios en la base de datos
         mydb.commit()
 
-        return f"La prenda agregada es: {producto},{cantidad}, {precio}, {talle}, {colores}"
+        return f"La prenda agregada es: {producto}, {cantidad}, {precio}, {talle}, {colores}"
     else:
         return "No se ha proporcionado ninguna prenda..."
 
@@ -82,14 +82,15 @@ def actualizar_producto(id):
     talle_valor = request.form['talle'] 
     color_nombre = request.form['colores']
 
-    sql = "UPDATE producto SET producto = %s, cantidad = %s, precio = %s, talle = %s, colores = %s WHERE id = %s"
-    datos = (id, producto_nombre, cantidad_valor, precio_valor, talle_valor, color_nombre)
+    # Actualización de la tabla 'Stock' con los datos proporcionados
+    sql = "UPDATE Stock SET producto = %s, cantidad = %s, precio = %s, talle = %s, colores = %s WHERE id = %s"
+    datos = (producto_nombre, cantidad_valor, precio_valor, talle_valor, color_nombre, id)
 
     cursor = mydb.cursor()
     cursor.execute(sql, datos)
     mydb.commit()
+    cursor.close()
     return redirect(url_for('mostrar_stock'))
-    #mydb.commit()
 
 @app.route('/mostrar', methods=['GET', 'POST'])
 def mostrar_stock():
@@ -97,37 +98,20 @@ def mostrar_stock():
     cursor.execute("SELECT * FROM Stock")
     datos_prenda = cursor.fetchall()
     cursor.close()
-    print(datos_prenda)
     return render_template('index.html', Stock=datos_prenda)
 
 @app.route('/eliminar', methods=['POST'])
 def eliminar():
     id = request.form.get('id')
     
-    # Usa la conexión ya existente sin llamarla como función
-    cursor = mydb.cursor()
-
     # Validación y ejecución de la eliminación
     if id:
+        cursor = mydb.cursor()
         cursor.execute("DELETE FROM Stock WHERE id = %s", (id,))
         mydb.commit()
-    
-    cursor.close()
-    return redirect(url_for('index'))
-
-"""
-@app.route('/eliminar', methods=['POST'])
-def eliminar():
-    id = request.form.get('id')  # Obtenemos el valor del campo "id" en el formulario
-    if id:  # Verificamos que el ID no esté vacío
-        conexion = mysql.connector.connect(mydb)
-        cursor = conexion.cursor()
-        cursor.execute("DELETE FROM stock WHERE id = %s", (id,))
-        conexion.commit()
         cursor.close()
-        conexion.close()
+    
     return redirect(url_for('index'))
-"""
 
 if __name__ == '__main__':
     app.run(debug=True)
